935e91cda99a8353cacd447c170433f60e405034
lcontento <lcontento@users.noreply.github.com>
2018-07-16 14:24:46 +0900

    Rewrite parse function
    Cases such as 1.0000001e6 are now handled correctly.
    scinote and parameters functions are no longer used.

--- src/decimal.jl
+++ src/decimal.jl
@@ -1,10 +1,22 @@
 # Convert a string to a decimal, e.g. "0.01" -> Decimal(0, 1, -2)
 function Base.parse(::Type{Decimal}, str::AbstractString)
-    if 'e' in str
-        return parse(Decimal, scinote(str))
+    # Read sign
+    s = (str[1] == '-') ? 1 : 0
+    # Unpack scientific notation
+    scno = split(str, 'e')
+    expo = length(scno) == 2 ? parse(Int64, scno[2]) : zero(Int64)
+    # Unpack coefficient and get fractional part
+    coef = split(scno[1], '.')
+    frac = length(coef) == 2 ? rstrip(coef[2], '0') : ""
+    # Update exponent
+    if length(frac) == 0
+        c = rstrip(coef[1], '0')
+        expo += length(coef[1]) - length(c)
+    else
+        c = coef[1] * frac
+        expo -= length(frac)
     end
-    c, q = parameters(('.' in str) ? split(str, '.') : str)
-    normalize(Decimal((str[1] == '-') ? 1 : 0, c, q))
+    Decimal(s, abs(parse(BigInt, c)), expo)
 end
 
 decimal(str::AbstractString) = parse(Decimal, str)
@@ -17,32 +29,6 @@ Decimal(num::Real) = parse(Decimal, string(num))
 Base.convert(::Type{Decimal}, num::Real) = Decimal(num::Real)
 decimal(x::Real) = Decimal(x)
 Decimal(x::Decimal) = x
-
-# Get Decimal constructor parameters from string
-parameters(x::AbstractString) = (abs(parse(BigInt, x)), 0)
-
-# Get Decimal constructor parameters from array
-function parameters(x::Array)
-    c = parse(BigInt, join(x))
-    (abs(c), -length(x[2]))
-end
-
-# Get decimal() argument from scientific notation
-function scinote(str::AbstractString)
-    s = (str[1] == '-') ? "-" : ""
-    n, expo = split(str, 'e')
-    n = split(n, '.')
-    if s == "-"
-        n[1] = n[1][2:end]
-    end
-    if parse(Int64, expo) > 0
-        shift = parse(Int64, expo) - ((length(n) == 2) ? length(n[2]) : 0)
-        s * join(n) * repeat("0", shift)
-    else
-        shift = -parse(Int64, expo) - ((length(n) == 2) ? length(n[1]) : length(n))
-        s * "0." * repeat("0", shift) * join(n)
-    end
-end
 
 # Convert a decimal to a string
 function Base.print(io::IO, x::Decimal)
