22643a467802104755b67fbdbdc3cc26fdbf4c3e
Nicola Mosco <nicola.mosco@gmail.com>
2023-04-14 10:14:03 +0200

    Supporting tryparse

--- src/decimal.jl
+++ src/decimal.jl
@@ -1,9 +1,18 @@
+@noinline function _parse_failure(T, str::AbstractString)
+    throw(ArgumentError("cannot parse $str as $T"))
+end
+
 # Convert a string to a decimal, e.g. "0.01" -> Decimal(0, 1, -2)
 function Base.parse(::Type{Decimal}, str::AbstractString)
+    @something tryparse(Decimal, str) _parse_failure(Decimal, str)
+end
+
+# Support for tryparse: useful also for integration with other packages, e.g. CSVFiles.jl, Parsers.jl
+function Base.tryparse(::Type{Decimal}, str::AbstractString)
     if 'e' in str
-        return parse(Decimal, scinote(str))
+        return tryparse(Decimal, _scinote_internal(str))
     end
-    c, q = parameters(('.' in str) ? split(str, '.') : str)
+    c, q = @something _parameters_internal(('.' in str) ? split(str, '.') : str) return
     normalize(Decimal((str[1] == '-') ? 1 : 0, c, q))
 end
 
@@ -15,8 +24,34 @@ Base.convert(::Type{Decimal}, num::Real) = Decimal(num::Real)
 decimal(x::Real) = Decimal(x)
 Decimal(x::Decimal) = x
 
+
+function _parameters_internal(x::AbstractString)
+    c = @something tryparse(BigInt, x) return
+    abs(c), 0
+end
+
+function _parameters_internal(x::Array)
+    c = @something tryparse(BigInt, join(x)) return
+    length(x) == 2 || return
+    abs(c), -length(x[2])
+end
+
+function _parameters_internal(x::AbstractString, raise::Bool)
+    @something _parameters_internal(x) begin
+        raise && _parse_failure(Decimal, x)
+        return
+    end
+end
+
+function _parameters_internal(x::Array, raise::Bool)
+    @something _parameters_internal(x) begin
+        raise && _parse_failure(Decimal, repr(x))
+        return
+    end
+end
+
 # Get Decimal constructor parameters from string
-parameters(x::AbstractString) = (abs(parse(BigInt, x)), 0)
+parameters(x::AbstractString) = _parameters_internal(x, true)
 
 # Get Decimal constructor parameters from array
 function parameters(x::Array)
@@ -24,21 +59,33 @@ function parameters(x::Array)
     (abs(c), -length(x[2]))
 end
 
-# Get decimal() argument from scientific notation
-function scinote(str::AbstractString)
+function _scinote_internal(str::AbstractString)
     s = (str[1] == '-') ? "-" : ""
     n, expo = split(str, 'e')
     n = split(n, '.')
     if s == "-"
         n[1] = n[1][2:end]
     end
-    if parse(Int64, expo) > 0
-        shift = parse(Int64, expo) - ((length(n) == 2) ? length(n[2]) : 0)
+    expo_ = @something tryparse(Int64, expo) return
+    if expo_ > 0
+        shift = expo_ - ((length(n) == 2) ? length(n[2]) : 0)
         s * join(n) * repeat("0", shift)
     else
-        shift = -parse(Int64, expo) - ((length(n) == 2) ? length(n[1]) : length(n))
+        shift = -expo_ - ((length(n) == 2) ? length(n[1]) : length(n))
         s * "0." * repeat("0", shift) * join(n)
     end
+end
+
+function _scinote_internal(str::AbstractString, raise::Bool)
+    @something _scinote_internal(str) begin
+        raise && throw(ArgumentError("cannot parse scientific notation for $str"))
+        return
+    end
+end
+
+# Get decimal() argument from scientific notation
+function scinote(str::AbstractString)
+    _scinote_internal(str, true)
 end
 
 # Convert a decimal to a string
