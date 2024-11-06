4d37211e55b36c3f168f49415ab94903a38972f7
Denis Baručić <barucden@fel.cvut.cz>
2024-11-06 17:01:14 +0100

    Fix conversion from floats
    Numbers such as `0.3` or `1.1` cannot be represented exactly using
    `Float32` or `Float64`. We can all digits by converting them to
    `BigFloat`:
    ```julia
    julia> big(0.3)
    0.299999999999999988897769753748434595763683319091796875
    
    julia> big(1.1)
    1.100000000000000088817841970012523233890533447265625
    ```
    This PR updates the `Decimal` constructors so that floating point
    numbers are converted to `Decimal`s exactly in the sense that
    ```julia
    julia> hash(1.1) == hash(Decimal(1.1))
    true
    ```
    
    Fixes #85

--- test/test_arithmetic.jl
+++ test/test_arithmetic.jl
@@ -4,41 +4,6 @@ using Test
 
 @testset "Arithmetic" begin
 
-@testset "Addition" begin
-    @test Decimal(0.1) + 0.2 == 0.1 + Decimal(0.2) == Decimal(0.1) + Decimal(0.2) == Decimal(0.3)
-    @test Decimal.([0.1 0.2]) .+ [0.3 0.1] == Decimal.([0.4 0.3])
-    @test Decimal(2147483646) + Decimal(1) == Decimal(2147483647)
-    @test Decimal(1,3,-2) + parse(Decimal, "0.2523410412138103") == Decimal(false,2223410412138103,-16)
-    @test Decimal(0, 10000000000000000001, -19) + Decimal(0, 1, 0) == Decimal(0, 20000000000000000001, -19)
-end
-
-@testset "Subtraction" begin
-    @test Decimal(0.3) - 0.1 == 0.3 - Decimal(0.1)
-    @test 0.3 - Decimal(0.1) == Decimal(0.3) - Decimal(0.1)
-    @test Decimal(0.3) - Decimal(0.1) == Decimal(0.2)
-    @test Decimal.([0.3 0.1]) .- [0.1 0.5] == Decimal.([0.2 -0.4])
-end
-
-@testset "Negation" begin
-    @test -Decimal.([0.3 0.2]) == [-Decimal(0.3) -Decimal(0.2)]
-    @test -Decimal(0.3) == zero(Decimal) - Decimal(0.3)
-    @test iszero(Decimal(12.1) - Decimal(12.1))
-end
-
-@testset "Multiplication" begin
-    @test Decimal(12.21) * Decimal(2.12) == Decimal(0,258852,-4)
-    @test Decimal(12.2112543) * Decimal(2.121352) == Decimal(0,259043687318136,-13)
-    @test Decimal(0.2) * 0.1 == 0.2 * Decimal(0.1)
-    @test 0.2 * Decimal(0.1) == Decimal(0.02)
-    @test Decimal(12.34) * 0.1234 == 12.34 * Decimal(0.1234)
-    @test 12.34 * Decimal(0.1234) == Decimal(1.522756)
-    @test Decimal(0.21084210) * -2 == -2 * Decimal(0.21084210)
-    @test -2 * Decimal(0.21084210) == Decimal(-0.4216842)
-    @test Decimal(0, 2, -1) * 0.0 == zero(Decimal)
-    @test Decimal.([0.3, 0.6]) .* 5 == [Decimal(0.3)*5, Decimal(0.6)*5]
-    @test one(Decimal) * 1 == Decimal(0, 1, 0)
-end
-
 @testset "Inversion" begin
     @with_context (Emax=384, Emin=-383, precision=9, rounding=RoundNearestTiesAway) @test inv(dec"1") == dec"1"
     @with_context (Emax=384, Emin=-383, precision=9, rounding=RoundNearestTiesAway) @test inv(dec"2") == dec"0.5"
@@ -106,13 +71,6 @@ end
     @with_context (Emax=999, Emin=-999, precision=7, rounding=RoundNearestTiesAway) @test inv(dec"9.9") == dec"0.1010101"
     @with_context (Emax=999, Emin=-999, precision=6, rounding=RoundNearestTiesAway) @test inv(dec"9.9") == dec"0.101010"
     @with_context (Emax=999, Emin=-999, precision=9, rounding=RoundNearestTiesAway) @test inv(dec"2") == dec"0.5"
-end
-
-@testset "Division" begin
-    @test Decimal(0.2) / Decimal(0.1) == Decimal(2)
-    @test Decimal(0.3) / Decimal(0.1) == Decimal(0,3,0)
-    @test [Decimal(0.3) / Decimal(0.1), Decimal(0.6) / Decimal(0.1)] == [Decimal(0.3), Decimal(0.6)] ./ Decimal(0.1)
-    @test [Decimal(0.3) / 0.1, Decimal(0.6) / 0.1] == [Decimal(0.3), Decimal(0.6)] ./ 0.1
 end
 
 end
